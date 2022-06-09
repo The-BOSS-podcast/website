363131cc42384938ef1d312833913406a172e536
Alok Prateek <oxyenyos@gmail.com>
2022-06-09 20:13:55 +0530

    add nextjs stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,28 @@
 
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
+          <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-JVXZ1REBPB`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JVXZ1REBPB', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     <PlasmicRootProvider>
-      <Head>
-        <script async src=“https://www.googletagmanager.com/gtag/js?id=G-JVXZ1REBPB”></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag(‘js’, new Date());
-          gtag(‘config’, ‘G-JVXZ1REBPB’);
-        </script>
-      </Head>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
