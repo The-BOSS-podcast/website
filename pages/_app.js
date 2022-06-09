6709aad92fc351afbad353f70a425c48f584151d
Alok Prateek <oxyenyos@gmail.com>
2022-06-09 19:41:08 +0530

    add GA4

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,16 @@ import { PlasmicRootProvider } from "@plasmicapp/react-web";
 function MyApp({ Component, pageProps }) {
   return (
     <PlasmicRootProvider>
+      <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src=“https://www.googletagmanager.com/gtag/js?id=G-JVXZ1REBPB”></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag(‘js’, new Date());
+          gtag(‘config’, ‘G-JVXZ1REBPB’);
+        </script>
+      </Head>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
