856b6c6c59834c0890993bd1a239cc906eb97cfc
Alok Prateek <i@alokprateek.in>
2022-06-15 15:42:57 +0530

    fix build

--- pages/_app.js
+++ pages/_app.js
@@ -5,23 +5,7 @@ import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-          <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-JVXZ1REBPB`}
-      />
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JVXZ1REBPB', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+          
     <PlasmicRootProvider>
       <Component {...pageProps} />
     </PlasmicRootProvider>
