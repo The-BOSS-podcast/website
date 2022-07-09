1cade14e13e5aeb09d8c2810a1449608e3ff4c58
Alok Prateek <oxyenyos@gmail.com>
2022-07-09 18:35:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   trailingSlash: true,
   // Your NextJS config.
 };
